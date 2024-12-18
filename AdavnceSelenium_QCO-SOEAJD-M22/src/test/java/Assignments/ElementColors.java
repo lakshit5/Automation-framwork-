388fd2278ec4676c40c2a281072d2df2f5cfd9ac
sanjaygit1 <152388009+sanjaygit1@users.noreply.github.com>
2024-12-18 11:18:26 +0530

    Update ElementColors.java

--- AdavnceSelenium_QCO-SOEAJD-M22/src/test/java/Assignments/ElementColors.java
+++ AdavnceSelenium_QCO-SOEAJD-M22/src/test/java/Assignments/ElementColors.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class ElementColors {
-
+//pull back
 	public static void main(String[] args) {
 		
 		WebDriver driver=new ChromeDriver();
