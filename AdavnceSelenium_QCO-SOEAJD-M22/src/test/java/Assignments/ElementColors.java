a706fb7f81ad54f134809b9c836c4ba3cadb19f9
Divya Jyothi <Divya Jyothi@DivyaJyothi>
2024-12-18 12:28:16 +0530

    CommitDj2

--- AdavnceSelenium_QCO-SOEAJD-M22/src/test/java/Assignments/ElementColors.java
+++ AdavnceSelenium_QCO-SOEAJD-M22/src/test/java/Assignments/ElementColors.java
@@ -11,7 +11,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class ElementColors {
 //pull back
 	//DJ1 pushing
-	//DJ2 pushing
+	//DJ2 pushing 18Dec
 	public static void main(String[] args) {
 		
 		WebDriver driver=new ChromeDriver();
